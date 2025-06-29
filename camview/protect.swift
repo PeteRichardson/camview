e4dfbf0121cd8572969da9611593e5000557a9f2
Pete Richardson <pete@peterichardson.com>
2025-06-29 11:26:31 -0700

    refactor: remove fetch.swift.    One function, so just move it to protect.swift

--- camview/protect.swift
+++ camview/protect.swift
@@ -10,7 +10,25 @@ import Logging
 
 var logger = Logger(label: "protect")
 
-struct Viewport: Decodable {
+
+func fetchJSON(from url: URL, headers: [String: String]) async throws -> Data {
+    var request = URLRequest(url: url)
+    headers.forEach { key, value in
+        request.setValue(value, forHTTPHeaderField: key)
+    }
+
+    let (data, response) = try await URLSession.shared.data(for: request)
+
+    guard let httpResponse = response as? HTTPURLResponse,
+          (200..<300).contains(httpResponse.statusCode) else {
+        throw URLError(.badServerResponse)
+    }
+
+    return data
+}
+
+
+struct Viewport: Decodable, Comparable {
     var id: String
     var liveview: String
     var modelKey: String
